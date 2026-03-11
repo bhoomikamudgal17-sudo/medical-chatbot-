7800eb25d423f2e89883a3cec9abeecdeef3425d
bhoom <bhoomikamudgal17@gmail.com>
2026-03-11 22:07:38 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,11 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/trials.ipynb
+touch requirements.txt
+touch .env
+touch setup.py
+touch app.py
+echo "success"
